3fa487736ffdf546e030de8567b64179cfa52408
PentaTrax <philipnordnielsen@gmail.com>
2024-09-23 10:46:17 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DaniaProjects
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool running = true;
+            int num1 = 0;
+            int num2 = 0;
+            int sum = 0;
+            string input1 = "";
+            string input2 = "";
+            char finalInput;
+            do
+            {
+                Console.Clear();
+                Console.WriteLine("Hello, World!");
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.WriteLine("Type a number:");
+                input1 = Console.ReadLine();
+                Console.Beep();
+                Console.WriteLine("Type a number to add:");
+                input2 = Console.ReadLine();
+                Console.Beep();
+                if (input1 != "" && input2 != "")
+                {
+                    num1 = Convert.ToInt32(input1);
+                    num2 = Convert.ToInt32(input2);
+                    sum = num1 + num2;
+                }
+                Console.WriteLine("The sum of your numbers is: " + sum);
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.WriteLine("Press ESC to close or ENTER to go again");
+            } while (Console.ReadKey(true).Key != ConsoleKey.Escape);
+            Environment.Exit(0);
+        }
+    }
+}
